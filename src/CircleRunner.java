ca5af213b4596bb1e8482d2d0d1589153acca408
Andy Liang <andyl6@nycstudents.net>
2022-09-30 11:36:59 -0400

    Circle

---
+++ src/CircleRunner.java
@@ -0,0 +1,17 @@
+public class CircleRunner {
+    public static void main(String[] args)
+    {
+        //A. create a Circle object with radius 5.0
+        Circle circle1 = new Circle (5);
+        //B. locate the method defined in the Circle class and write
+        //   a line of code to call it on your object
+        circle1.printArea();
+
+        //C. BEFORE running this code, PREDICT what gets printed
+
+        //PREDICTION: It will print 5*5*3.14
+
+        //D. run the code and confirm your prediction! Were you correct?
+        // YES
+    }
+}
